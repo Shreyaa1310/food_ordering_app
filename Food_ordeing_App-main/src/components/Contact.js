cc1ecc3e49165c94cadfd73670e04da696b9dfef
Shreya <shreya.m@kynhood.com>
2024-12-17 00:16:32 +0530

    food_ordering_app

---
+++ Food_ordeing_App-main/src/components/Contact.js
@@ -0,0 +1,15 @@
+const Contact = ()=>{
+    return(
+        <div className="mt-72 w-10/12 m-auto border border-orange-200 p-3 rounded-lg">
+        <h1 className="text-center mb-6 text-4xl font-extrabold">Contact <span className="text-orange-500">Me</span></h1>
+        <div className="text-center">
+        <h3 className="mb-3"> <label className="font-bold text-orange-400">Name :</label> Shreya Monga</h3>
+        <h3 className="mb-3"> <label className="font-bold text-orange-400">Address :</label> New Delhi, India</h3>
+        <h3 className="mb-3"><label className="font-bold text-orange-400">Email :</label> shreya.monga98@gmail.com</h3>
+        <h3 className="mb-3"><label className="font-bold text-orange-400">LinkedIn :</label> www.linkedin.com/in/shreya-monga</h3>
+        <h3 className="mb-3"><label className="font-bold text-orange-400">Github :</label> https://github.com/Shreyaa1310</h3>
+        </div>
+        </div>
+    )
+}
+export default Contact;
---
+++ Food_ordeing_App-main/src/components/User_card.js
@@ -0,0 +1,11 @@
+const User_card = ()=>{
+    return(
+        <div className="user">
+        <h2>name:shreya</h2>
+        <h3>location:xxxxxx</h3>
+        <h3>contact:shreya.monga98@gmail.com</h3>
+        </div>
+    )
+}
+
+export default User_card;
